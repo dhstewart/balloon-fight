f2bade5ca8a089826fdf7e5696db41de42e88772
Hunter Stewart <hetre85@gmail.com>
2015-08-16 15:39:36 -0500

    Rotate horizontal orientation programmitcally based on direction player walks.

--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -5,6 +5,7 @@ public class player_movement : MonoBehaviour {
 
 	Animator anim;
 	public float speed = 1f;
+	public string playerFacing = "right";
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
@@ -13,10 +14,24 @@ public class player_movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.A)) {
+			if (playerFacing == "right")
+			{
+				playerFacing = "left";
+			    Vector2 theScale = transform.localScale;
+			    theScale.x *= -1;
+			    transform.localScale = theScale;
+			}
 			transform.Translate(Vector2.left * speed * Time.deltaTime);
 			anim.SetBool("walking", true);
 		}
 		else if (Input.GetKey (KeyCode.D)) {
+			if (playerFacing == "left")
+			{
+				playerFacing = "right";
+				Vector2 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;
+			}
 			transform.Translate(Vector2.right * speed * Time.deltaTime);
 			anim.SetBool("walking", true);
 		}
