4103273c61427fe8fe565259b5a45cf9d3431d2f
Hunter Stewart <hetre85@gmail.com>
2015-08-16 15:27:04 -0500

    make animation start and stop on key press.

--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -3,19 +3,28 @@ using System.Collections;
 
 public class player_movement : MonoBehaviour {
 
+	Animator anim;
 	public float speed = 1f;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.A)) {
-			transform.Translate(Vector2.left * speed * Time.deltaTime);    
+			transform.Translate(Vector2.left * speed * Time.deltaTime);
+			anim.SetBool("walking", true);
 		}
 		else if (Input.GetKey (KeyCode.D)) {
-			transform.Translate(Vector2.right * speed * Time.deltaTime);   
+			transform.Translate(Vector2.right * speed * Time.deltaTime);
+			anim.SetBool("walking", true);
+		}
+
+		if(Input.anyKey == false)
+		{
+			anim.SetBool("walking", false);
 		}
 	}
+
 }
