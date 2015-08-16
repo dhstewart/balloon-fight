6727e7f38d922c73a1a6d8dee1a03e530ced1e0a
Hunter Stewart <hetre85@gmail.com>
2015-08-16 11:48:44 -0500

    Added Scripts folder. Player can now move left and right.

---
+++ Assets/scripts/player_movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_movement : MonoBehaviour {
+
+	public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate(Vector2.left * speed * Time.deltaTime);    
+		}
+		else if (Input.GetKey (KeyCode.D)) {
+			transform.Translate(Vector2.right * speed * Time.deltaTime);   
+		}
+	}
+}
